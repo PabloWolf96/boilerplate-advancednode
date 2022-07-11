36989d4b797bba3e4a9f99d793bf633d158cd745
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-11 15:48:37 +0100

    add logout route and 404

--- server.js
+++ server.js
@@ -88,6 +88,13 @@ app.get("/profile", ensureAuthenticated, (req, res) => {
     username: req.user.username,
   });
 });
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+app.use((req, res, next) => {
+  res.status(404).type("text").send("Not Found");
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
